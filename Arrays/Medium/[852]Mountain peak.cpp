b02fcdd03a3268a375a6102655a25a23454f8361
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-04 10:38:20 +0530

    Update [852]Mountain peak.cpp

--- Arrays/Medium/[852]Mountain peak.cpp
+++ Arrays/Medium/[852]Mountain peak.cpp
@@ -6,14 +6,14 @@ public:
         while (start<=end){
            int  mid = start + (end- start )/2;
             
-
+             // cheacking if mid to nahi hai peak element 
             if (arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1])
             {
                 return mid;
 
             } else if (arr[mid-1]<arr[mid]) // for right side search bcz element is smaller than mid mtlb bada element khi right me exist karega 
             {
-                start = mid +1;
+                start = mid +1; // seaerrch in right side of array 
             }
             else{
                 end = mid -1;
