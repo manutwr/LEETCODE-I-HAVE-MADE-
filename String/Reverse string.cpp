8f756a77be1f9b5798e5a5ca862477c6fce2bf33
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-09 10:25:57 +0530

    Update Reverse string.cpp

--- String/Reverse string.cpp
+++ String/Reverse string.cpp
@@ -3,7 +3,7 @@ public:
     void reverseString(vector<char>& s) {
         int start = 0 , end = s.size ()-1;// 0 to n-1 
         while (start<=end){//two pointers approach swap karna hota h first element to last-1; ir yhi process tb tk karte hai jab tak start se bada end na ho jaye (start<=end);
-            swap(s[start++], s[end--]);
+            swap(s[start++], s[end--]);// start ko badha do end ko kam kr do aur yhi process loop pe kro
         }
     }
 };
