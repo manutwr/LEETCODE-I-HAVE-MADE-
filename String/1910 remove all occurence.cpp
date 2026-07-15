bd0f539f0cc298d3bd43c644c80559a62bae5f47
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-15 10:24:17 +0530

    Update 1910 remove all occurence.cpp

--- String/1910 remove all occurence.cpp
+++ String/1910 remove all occurence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
         while (s.length () > 0 &&  s.find( part )< s.length ()){ // cheaking that string is valid or not
-            s.erase (s.find (part), part.length());
+            s.erase (s.find (part), part.length()); // erasing the string part and the lenght of part
         }
         return s;
     }
