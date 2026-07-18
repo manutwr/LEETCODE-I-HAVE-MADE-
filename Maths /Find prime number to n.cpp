154a2f5cfe13738798fc24fa654b5af236e86979
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-18 09:50:51 +0530

    Update Find prime number to n.cpp

--- Maths /Find prime number to n.cpp
+++ Maths /Find prime number to n.cpp
@@ -5,7 +5,7 @@ public:
        int count = 0; // initialise count with 0
       
      for (int i = 2 ; i<n ; i++){ // loop for cheacking prime 
-        if (isPrime [i]){
+        if (isPrime [i]){// cheacking if prime or not 
             count ++;
 
             for (int j = i*2;  j<n ; j+=i){
