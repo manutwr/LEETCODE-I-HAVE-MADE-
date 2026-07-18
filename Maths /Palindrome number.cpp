f3a646c30cc467ec29d61e2736299984008567c7
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-18 10:03:13 +0530

    Create Palindrome number.cpp

---
+++ Maths /Palindrome number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int ans = 0 , rem; // continer 
+        
+        while(x!=0) // agar x zero ke barabar nahi hai
+        {
+            
+            rem = x%10;
+            x/=10;
+            if(ans>INT_MAX/10 || ans<INT_MIN/10)
+            return 0 ;
+            ans = ans*10+rem;
+        }
+        return ans;
+    }
+    bool isPalindrome (int n ){
+        if (n<0) return false ;
+        int ans = reverse (n);
+        return n == ans ;
+    }
+};
