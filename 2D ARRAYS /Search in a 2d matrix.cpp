627e9ff541da2f2f58782827a51c438522268bb8
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-07-24 09:38:37 +0530

    Update Search in a 2d matrix.cpp

--- 2D ARRAYS /Search in a 2d matrix.cpp
+++ 2D ARRAYS /Search in a 2d matrix.cpp
@@ -3,8 +3,8 @@ public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         int m = mat.size() , n = mat[0].size();  // m= row size , n= column size 
 
-        int r = 0, c =n-1;
-        while (r<m && c>=0){
+        int r = 0, c =n-1;  // oo se start kia aur n-1 ko pe row and column ko kia
+        while (r<m && c>=0){ // hile loop
             if (target == mat [r][c]){
                 return true;
             }
