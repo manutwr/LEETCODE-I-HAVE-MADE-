9704fdaefef3f5f37fab8af12b68f28152f2f641
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-09 09:22:25 +0530

    Update Rotated arrays.cpp

--- Arrays/Medium/Rotated arrays.cpp
+++ Arrays/Medium/Rotated arrays.cpp
@@ -10,7 +10,7 @@ public:
                 return mid;
             }
 
-            if (nums[st] <= nums[mid]) {
+            if (nums[st] <= nums[mid]) { 
                 if (nums[st] <= target && target < nums[mid]) {
                     end = mid - 1;
                 } else {
