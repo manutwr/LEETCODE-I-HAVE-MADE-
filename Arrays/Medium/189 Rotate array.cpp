b35a0e5a157ab837f07a980af218c86d4ec0d8f2
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-17 08:10:35 +0530

    Update 189 Rotate array.cpp

--- Arrays/Medium/189 Rotate array.cpp
+++ Arrays/Medium/189 Rotate array.cpp
@@ -13,6 +13,6 @@ public:
         k= k%n;  // overflow nah ho code baar baar rotate h ho array ni to woh fir se vaisa hi bn jayega jaisa phle tha 
         reverse (arr, 0 , n-k-1); // phla fucction array ko 0 se n-k-1 tak reverse krna 
         reverse ( arr , n-k ,n-1); // dusra n-k se n-1 ko rotte krna kyuki jo part aage le jaana hai usee reverse krna 
-        reverse (arr, 0, n-1); // pure array ko veerse krna 
+        reverse (arr, 0, n-1); // pure array ko veerse krna  
     }
 };
