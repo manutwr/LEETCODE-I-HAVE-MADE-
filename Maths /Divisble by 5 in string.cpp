6ac58449b48de0aadbb71a17632b203a350d2cd4
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-19 06:44:50 +0530

    Update Divisble by 5 in string.cpp

--- Maths /Divisble by 5 in string.cpp
+++ Maths /Divisble by 5 in string.cpp
@@ -5,7 +5,7 @@ public:
         char lastChar = n.back(); // last charecter ko identify kro
 
         
-        if (lastChar == '0' || lastChar == '5') {
+        if (lastChar == '0' || lastChar == '5') { // agar last charecter 5 y 0 ho 
             return true;
         }
 
