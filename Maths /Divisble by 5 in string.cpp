5f555ef6d982561fc16c936ab4c269bf4ac1f85a
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-19 06:42:44 +0530

    Update and rename Divisble by 5 in string,cpp to Divisble by 5 in string.cpp

--- Maths /Divisble by 5 in string.cpp
+++ Maths /Divisble by 5 in string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool divisibleBy5(string &n) {
        
-        char lastChar = n.back();
+        char lastChar = n.back(); // last charecter ko identify kro
 
         
         if (lastChar == '0' || lastChar == '5') {
