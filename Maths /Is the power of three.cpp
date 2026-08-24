6d0e745713e5f09475d3b5befadec79c859b82c4
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-24 21:34:37 +0530

    Update Is the power of three.cpp

--- Maths /Is the power of three.cpp
+++ Maths /Is the power of three.cpp
@@ -8,7 +8,7 @@ public:
 
        
         while (n % 3 == 0) { // while lop to cheaking the divisbliy by two
-            n = n / 3; // n ko 2 se devide krke lopp me daal do agar divide hne ke baad ek sbs chhota hissa 2 se divible ho gyaa to 2 ki power hai
+            n = n / 3; // n ko 3 se devide krke lopp me daal do agar divide hne ke baad ek sbs chhota hissa 3 se divible ho gyaa to 2 ki power hai
         }
 
     
