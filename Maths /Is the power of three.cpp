38d1ed692c9b82ecdbeaed1146f44a21dec1f132
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-24 21:34:12 +0530

    Create Is the power of three.cpp

---
+++ Maths /Is the power of three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+     
+        if (n <= 0) { // if n is less than 0 then false return 
+            return false;
+        }
+
+       
+        while (n % 3 == 0) { // while lop to cheaking the divisbliy by two
+            n = n / 3; // n ko 2 se devide krke lopp me daal do agar divide hne ke baad ek sbs chhota hissa 2 se divible ho gyaa to 2 ki power hai
+        }
+
+    
+        return n == 1; // if not any output then return 1 
+    }
+};
