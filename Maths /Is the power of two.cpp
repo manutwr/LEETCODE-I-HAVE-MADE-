7e25c5d0b76f5094320d7df240503d9ac6321de8
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-24 21:32:40 +0530

    Update Is the power of two.cpp

--- Maths /Is the power of two.cpp
+++ Maths /Is the power of two.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
      
-        if (n <= 0) {
+        if (n <= 0) { // if n is less than 0 then false return 
             return false;
         }
 
        
-        while (n % 2 == 0) {
-            n = n / 2;
+        while (n % 2 == 0) { // while lop to cheaking the divisbliy by two
+            n = n / 2; // n ko 2 se devide krke lopp me daal do agar divide hne ke baad ek sbs chhota hissa 2 se divible ho gyaa to 2 ki power hai
         }
 
     
-        return n == 1;
+        return n == 1; // if not any output then return 1 
     }
 };
