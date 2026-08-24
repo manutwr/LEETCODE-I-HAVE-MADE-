ccce8fd70d4b24452047f2c851698e14cc6bee9d
Manu tiwari <sakshamtiwari.engg@gmail.com>
2026-08-24 21:15:56 +0530

    Update Fibocanni number.cpp

--- RECURSION/Fibocanni number.cpp
+++ RECURSION/Fibocanni number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0 || n == 1){
+        if (n == 0 || n == 1){ // base cases important for recursion
         return n;
         }
-        return fib(n-1) + fib(n-2);
+        return fib(n-1) + fib(n-2); // simple formula recursion of fiboconni numbers 
     }
 };
